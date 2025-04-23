9c5d82b426657255446eabd1804121b5c5a9d247
Ridham2345 <idhamcholidrdn@gmail.con>
2025-04-23 22:11:06 +0700

    refactor: extract class PersonalInfo untuk hilangkan data clumps dan long parameter list

---
+++ 08_Assessment_Tugas_Besar_Pengganti_UTS/src/model/PersonalInfo.java
@@ -0,0 +1,35 @@
+public class PersonalInfo {
+    private String namaLengkap;
+    private String alamat;
+    private String nomorTelepon;
+    private String email;
+    private String jenisKelamin;
+
+    public PersonalInfo(String namaLengkap, String alamat, String nomorTelepon, String email, String jenisKelamin) {
+        this.namaLengkap = namaLengkap;
+        this.alamat = alamat;
+        this.nomorTelepon = nomorTelepon;
+        this.email = email;
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    public String getNamaLengkap() {
+        return namaLengkap;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public String getNomorTelepon() {
+        return nomorTelepon;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+}
